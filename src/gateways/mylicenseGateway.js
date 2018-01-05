d724b63bfdef9126c060bfae6f6212e382ebdca8
Georgii Oleinikov <olgeorge@gmail.com>
2018-01-05 20:46:32 +0200

    Minor refactoring

--- src/gateways/mylicenseGateway.js
+++ src/gateways/mylicenseGateway.js
@@ -1,19 +1,11 @@
 const _ = require('lodash');
 const { Loggable } = require('../common/aspects');
 const mylicenseClient = require('../clients/mylicenseClient');
-const axios = require('axios');
 const cheerio = require('cheerio');
-const FormData = require('form-data');
 const setCookie = require('set-cookie-parser');
 const querystring = require('querystring');
-const request = require('superagent');
 
 const { MYLICENSE_ENDPOINT } = process.env;
-
-const getSearchWebpage = async () => {
-    const response = await mylicenseClient.get('/everification/Search.aspx');
-    return response.data;
-};
 
 const getSelectOptionsFromPage = async (webPage, selectName) => {
     const $ = cheerio.load(webPage);
@@ -48,7 +40,85 @@ const getViewStateFromPage = (webPage) => {
     const $ = cheerio.load(webPage);
     const __VIEWSTATE = $(`input[name=__VIEWSTATE]`).val();
     const __VIEWSTATEGENERATOR = $(`input[name=__VIEWSTATEGENERATOR]`).val();
-    return { __VIEWSTATE, __VIEWSTATEGENERATOR }
+    return { __VIEWSTATE, __VIEWSTATEGENERATOR };
+};
+
+const getSearchWebpage = async () => {
+    const response = await mylicenseClient.get('/everification/Search.aspx');
+    return response.data;
+};
+
+const getSearchCookies = async () => {
+    const response = await mylicenseClient.get('/everification/Search.aspx');
+    const cookies = setCookie.parse(response.headers['set-cookie']);
+    return _.reduce(cookies, (result, cookie) => {
+        return result + `${cookie.name}=${cookie.value}; `;
+    }, "");
+};
+
+const postRequest = async (url, data, Cookie) => {
+    const postData = querystring.stringify(data);
+    return mylicenseClient.post(url, postData, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': postData.length,
+            Cookie,
+        },
+    });
+};
+
+const executeSearchAndGetSearchPage = async ({
+    profession,
+    licenseType,
+    firstOrMidName,
+    lastName,
+    licenseNumber,
+    status,
+    county,
+    city,
+    state,
+    zipcode,
+    Cookie,
+}) => {
+    const response = await postRequest('/everification/Search.aspx', {
+        "__EVENTTARGET": "",
+        "__EVENTARGUMENT": "",
+        "t_web_lookup__profession_name": profession,
+        "t_web_lookup__license_type_name": licenseType,
+        "t_web_lookup__first_name": firstOrMidName,
+        "t_web_lookup__last_name": lastName,
+        "t_web_lookup__license_no": licenseNumber,
+        "t_web_lookup__license_status_name": status,
+        "t_web_lookup__addr_county": county,
+        "t_web_lookup__addr_city": city,
+        "t_web_lookup__addr_state": state,
+        "t_web_lookup__addr_zipcode": zipcode,
+        "sch_button": "Search",
+    }, Cookie);
+    if (!response.request || !response.request.path || response.request.path !== '/everification/SearchResults.aspx') {
+        throw new Error("Failure to fetch search results for the given query");
+    }
+    return response.data;
+};
+
+const getSearchResults = async ({
+    __VIEWSTATE,
+    __VIEWSTATEGENERATOR,
+    pageNumber,
+    Cookie,
+}) => {
+    const response = await postRequest('/everification/SearchResults.aspx', {
+        "CurrentPageIndex": 1,
+        "__EVENTTARGET": `datagrid_results:_ctl44:_ctl${pageNumber - 1}`,
+        "__EVENTARGUMENT": "",
+        __VIEWSTATE,
+        __VIEWSTATEGENERATOR,
+    }, Cookie);
+    const { items, numPages } = getSearchResultsFromPage(response.data);
+    if (pageNumber > numPages) {
+        return { items: [], numPages };
+    }
+    return { items, numPages };
 };
 
 class MylicenseGateway {
@@ -87,58 +157,25 @@ class MylicenseGateway {
         zipcode = "",
         pageNumber = 1,
     }) {
-        let response;
-        response = await mylicenseClient.get('/everification/Search.aspx');
-        const cookies = setCookie.parse(response.headers['set-cookie']);
-        const Cookie = _.reduce(cookies, (result, cookie) => {
-            return result + `${cookie.name}=${cookie.value}; `;
-        }, "");
-
-        let postData;
-        postData = querystring.stringify({
-            "__EVENTTARGET": "",
-            "__EVENTARGUMENT": "",
-            "t_web_lookup__profession_name": profession,
-            "t_web_lookup__license_type_name": licenseType,
-            "t_web_lookup__first_name": firstOrMidName,
-            "t_web_lookup__last_name": lastName,
-            "t_web_lookup__license_no": licenseNumber,
-            "t_web_lookup__license_status_name": status,
-            "t_web_lookup__addr_county": county,
-            "t_web_lookup__addr_city": city,
-            "t_web_lookup__addr_state": state,
-            "t_web_lookup__addr_zipcode": zipcode,
-            "sch_button": "Search",
+        const Cookie = await getSearchCookies();
+        const webPage = await executeSearchAndGetSearchPage({
+            profession,
+            licenseType,
+            firstOrMidName,
+            lastName,
+            licenseNumber,
+            status,
+            county,
+            city,
+            state,
+            zipcode,
+            Cookie,
         });
-        response = await mylicenseClient.post('/everification/Search.aspx', postData, {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': postData.length,
-                Cookie,
-            },
+        const { items, numPages } = await getSearchResults({
+            ...getViewStateFromPage(webPage),
+            pageNumber,
+            Cookie,
         });
-
-        if (!response.request || !response.request.path || response.request.path !== '/everification/SearchResults.aspx') {
-            throw new Error("Failure to fetch search results for the given query");
-        }
-
-        postData = querystring.stringify({
-            "CurrentPageIndex": 1,
-            "__EVENTTARGET": `datagrid_results:_ctl44:_ctl${pageNumber-1}`,
-            "__EVENTARGUMENT": "",
-            ...getViewStateFromPage(response.data),
-        });
-        response = await mylicenseClient.post('/everification/SearchResults.aspx', postData, {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': postData.length,
-                Cookie,
-            },
-        });
-        const { items, numPages } = getSearchResultsFromPage(response.data);
-        if (pageNumber > numPages) {
-            return { items: [], pageNumber, numPages };
-        }
         return { items, pageNumber, numPages };
     }
 }
